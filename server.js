abc1fe8b7a8996e3946d668765a54b6e692b9eeb
unknown <Sayan>
2017-12-27 00:48:24 +0530

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 
@@ -30,6 +31,6 @@ res.render('about.hbs',{
 
 });
 
-app.listen(3000,()=> {
-  console.log('Server is uptp 3000');
+app.listen(port,()=> {
+  console.log(`Server is uptp ${port}`);
 });
